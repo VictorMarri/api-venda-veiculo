b1900a7229d2a8fcf7afba84bec55f1cbc9ae495
VictorMarri <victorpujoni@gmail.com>
2021-03-11 16:01:46 -0300

    Classe BaseEntity modificada para Abstrata

--- src/Api.VendaVeiculo.Domain/Entities/Base/BaseEntity.cs
+++ src/Api.VendaVeiculo.Domain/Entities/Base/BaseEntity.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 
 namespace Api.VendaVeiculo.Domain.Entities.Base
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         [JsonIgnore]
         [Column("id")]
